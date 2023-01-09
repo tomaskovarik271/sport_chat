732400ec66eaa620e1730efe1b1c49edc79a3909
tokovarik <64414613+tokovarik@users.noreply.github.com>
2023-01-09 11:43:51 +0100

    add fixed prompt

--- server/server.js
+++ server/server.js
@@ -17,19 +17,20 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello from Sport Chat!'
   })
 })
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
+    const fixedString = "You are a professional trainer. My name is Martin, 41 years, 205 cm height, 105 kg weight. I want to improve my endurance. Generate professional training plan for one week.";
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `${fixedString} ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 300, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 1000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
